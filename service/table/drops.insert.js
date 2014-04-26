aca4571ec80561ce92e1a6dc2f121f26b419deee
windowsazure <windowsazure>
2014-04-26 21:12:03 +0000

    Committing update from request /scmvfs/service/table/drops.insert.js

--- service/table/drops.insert.js
+++ service/table/drops.insert.js
@@ -1,22 +1,19 @@
 function insert(item, user, request) {
     var queryString = "INSERT INTO drops (message, picture, location, owner) VALUES (?, ?, geography::STPointFromText('POINT(' + ? + ' ' + ? + ')', 4326), ?)";
     
-    mssql.query(queryString, 
-        [item.message, item.picture, item.longitude.toString(), item.latitude.toString(), user.userId], 
-        {
-            success: function (results) {
-                request.respond(200, results);
+    mssql.query(queryString, [item.message, item.picture, item.longitude.toString(), item.latitude.toString(), user.userId], {
+        success: function (results) {
+            request.respond(200, results);
 
-                return;
-            },
+            return;
+        },
 
-            error: function (error) {
-                console.log("Error inserting drop: " + error)
+        error: function (error) {
+            console.log("Error inserting drop: " + error)
 
-                request.respond(500, "Error inserting drop: " + error);
+            request.respond(500, "Error inserting drop: " + error);
 
-                return;
-            }
+            return;
         }
-    );
+    });
 }
