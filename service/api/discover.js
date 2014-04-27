fd8cf370befd9e44265093bd6db57c0253202e96
windowsazure <windowsazure>
2014-04-27 01:32:09 +0000

    Committing update from request /scmvfs/service/api/discover.js

--- service/api/discover.js
+++ service/api/discover.js
@@ -1,5 +1,5 @@
 exports.get = function(request, response) {
-    var queryString = "SELECT id, message, location.Lat latitude, location.Long longitude FROM drops WHERE location.STDistance(geography::STPointFromText('POINT(' + ? + ' ' + ? + ')', 4326)) <= ?"
+    var queryString = "DECLARE @g geography = geography::STPointFromText('POINT(' + ? + ' ' + ? + ')', 4326); SELECT id, message, location.Lat latitude, location.Long longitude FROM drops WHERE location.STDistance(@g) <= ? ORDER BY SpatialLocation.STDistance(@g);"
     
     request.service.mssql.query(queryString, [request.query.longitude.toString(), request.query.latitude.toString(), 1000], { 
           success: function(results) { 
